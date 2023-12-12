5b382666f08d7d3cc364032503069c6325880e3e
kiraYuukiAsuna <kiraYuukiAsuna@outlook.com>
2023-12-12 17:10:01 +0800

    Add http reverse proxy support for all grpc service.

--- src/DBMS/go.mod
+++ src/DBMS/go.mod
@@ -6,6 +6,7 @@ require github.com/gin-gonic/gin v1.9.1
 
 require (
 	github.com/google/uuid v1.4.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1
 	github.com/robfig/cron/v3 v3.0.1
 	go.mongodb.org/mongo-driver v1.12.1
 	google.golang.org/grpc v1.59.0
@@ -26,12 +27,14 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -39,11 +42,12 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- src/gen.bat
+++ src/gen.bat
@@ -3,14 +3,22 @@ mkdir "DBMS/Generated"
 mkdir "DBMS/Generated/Message"
 mkdir "DBMS/Generated/Service"
 
-"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto --plugin=protoc-gen-go=Bin/protobuf/bin/protoc-gen-go.exe --plugin=protoc-gen-go-grpc=Bin/protobuf/bin/protoc-gen-go-grpc.exe  --go_out=DBMS/Generated --go-grpc_out=DBMS/Generated proto/Message/*
-"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto --plugin=protoc-gen-go=Bin/protobuf/bin/protoc-gen-go.exe --plugin=protoc-gen-go-grpc=Bin/protobuf/bin/protoc-gen-go-grpc.exe  --go_out=DBMS/Generated --go-grpc_out=DBMS/Generated proto/Service/*
+"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto proto/Message/* --plugin=protoc-gen-go=Bin/protobuf/bin/protoc-gen-go.exe --plugin=protoc-gen-go-grpc=Bin/protobuf/bin/protoc-gen-go-grpc.exe  --go_out=DBMS/Generated --go-grpc_out=DBMS/Generated
 
-move DBMS/Generated/DBMS/Generated/proto DBMS/Generated/proto
+"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto proto/Service/* --plugin=protoc-gen-go=Bin/protobuf/bin/protoc-gen-go.exe --plugin=protoc-gen-go-grpc=Bin/protobuf/bin/protoc-gen-go-grpc.exe  --go_out=DBMS/Generated --go-grpc_out=DBMS/Generated
 
-rmdir /s /q "DBMS/Generated/DBMS"
-rmdir /s /q "DBMS/Generated/Message"
-rmdir /s /q "DBMS/Generated/Service"
+"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto proto/Service/* --plugin=protoc-gen-grpc-gateway=Bin/protobuf/bin/protoc-gen-grpc-gateway.exe --grpc-gateway_out DBMS/Generated --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true
 
-"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto --cpp_out=Generated --plugin=protoc-gen-grpc=Bin/protobuf/bin/grpc_cpp_plugin.exe --grpc_out=Generated proto/Message/*
-"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto --cpp_out=Generated --plugin=protoc-gen-grpc=Bin/protobuf/bin/grpc_cpp_plugin.exe --grpc_out=Generated proto/Service/*
+"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto proto/Service/* -I=protoc-gen-openapiv2/options --plugin=protoc-gen-openapiv2=Bin/protobuf/bin/protoc-gen-openapiv2.exe --openapiv2_out apiref/openapiv2 --openapiv2_opt generate_unbound_methods=true
+
+rmdir /s /q "DBMS\Generated\proto
+move "DBMS\Generated\DBMS\Generated\proto" "DBMS\Generated\proto"
+move "DBMS\Generated\Service\Service.pb.gw.go" "DBMS\Generated\proto\service"
+rmdir /s /q "DBMS\Generated\DBMS
+rmdir /s /q "DBMS\Generated\Message
+rmdir /s /q "DBMS\Generated\Service"
+
+"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto proto/Message/* --cpp_out=Generated --plugin=protoc-gen-grpc=Bin/protobuf/bin/grpc_cpp_plugin.exe --grpc_out=Generated
+
+"Bin/protobuf/bin/protoc.exe" -I=Bin/protobuf/include -I=proto proto/Service/* --cpp_out=Generated --plugin=protoc-gen-grpc=Bin/protobuf/bin/grpc_cpp_plugin.exe --grpc_out=Generated
+
